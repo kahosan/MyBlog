d259a3891f7328197dd3bd9a96403d1ac9a3b496
kahosan <15116355657l@gamil.com>
2021-08-14 16:21:45 +0800

    Site updated: 2021-08-14 16:21:39

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,19 +1,113 @@
-((darkModeChange) => {
-    $('#darkmode').click(() => {
-        const darkStyle = "/css/style-dark.css";
-        var nowThemeMode = true;
+/**
+ * @Author: kaho
+ * @Date: 2021-07-03 09:46:00
+ * @Mail: kahosan@outlook.com
+ * @LastEditTime: 2021-08-10 00:23:51
+ */
 
-        if (document.getElementById('dark') == null) {
-            nowThemeMode = '';
-        }
+// 获取当前操作系统的主题模式
+const nowMode = "nowMode";
+const darkStyle = "/css/style-dark.css";
+const darkModeToggleBottonElement = document.getElementById("darkmode");
+const getModeFromSystem = () => window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+const getDarkStyleIsExist = () => document.getElementById("dark");
 
-        localStorage.nowThemeMode = nowThemeMode;
-        if (!nowThemeMode) {
-            linkTag = $('<link id="dark" href="' + darkStyle + '" rel="stylesheet" type="text/css" />');
-            $($('head')[0]).append(linkTag);
-            nowThemeMode = true;
-        } else {
-            $('#dark').remove();
-        }
-    });
-})();
+// 封装下 LocalStorage 的操作，以应对 HTML5 Storage 被禁用、localStorage 被写满、localStorage 实现不完整的情况
+// 来源 Sukka
+const setLS = (key, value) => {
+  try {
+    localStorage.setItem(key, value);
+  } catch (e) {}
+};
+
+const removeLS = (key) => {
+  try {
+    localStorage.removeItem(key);
+  } catch (e) {}
+};
+
+const getLS = (key) => {
+  try {
+    return localStorage.getItem(key);
+  } catch (e) {
+    return null // 与 localStorage 中没有找到对应 key 的行为一致
+  }
+};
+
+//反转样式
+const inverMode = {
+  dark: "light",
+  light: "dark"
+}
+
+
+const validColorModeKeys = {
+  dark: true,
+  light: true
+};
+
+// 加入夜间模式
+const addDarkMode = () => {
+  linkTag = document.createElement('link');
+
+  linkTag.id = 'dark';
+  linkTag.href = darkStyle;
+  linkTag.setAttribute('rel', 'stylesheet');
+
+  document.head.appendChild(linkTag);
+}
+
+// 删除夜间模式
+const deleteDarkMode = () => {
+  getDarkStyleIsExist().remove();
+}
+
+
+//应用夜间模式
+const applyCustomdDarkMode = mode => {
+  const currentMode = mode || getLS(nowMode);
+  
+  if (currentMode === "light" && !getDarkStyleIsExist()) {
+    addDarkMode();
+  } else if (currentMode === "dark" && getDarkStyleIsExist()) {
+    deleteDarkMode();
+  }
+}
+
+//获取转换后的当前模式
+const toggleMode = () => {
+  let currentMode = getLS(nowMode);
+
+  if (validColorModeKeys[currentMode]) {
+    currentMode = inverMode[currentMode];
+  } else if (currentMode === null) {
+    currentMode = inverMode[getModeFromSystem()];
+  } else {
+    return;
+  }
+
+  setLS(nowMode, currentMode);
+
+  return currentMode;
+}
+
+applyCustomdDarkMode();
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+  if (event.matches) {
+    if (getDarkStyleIsExist()) {
+      return;
+    } else {
+      addDarkMode();
+      setLS(nowMode, "light");
+    }
+  } else {
+    if (!getDarkStyleIsExist()) {
+      return;
+    } else {
+      deleteDarkMode();
+      setLS(nowMode, "dark");
+    }
+
+  }
+});
