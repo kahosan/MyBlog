ef160a68a51679e0c87fb0d0a75ee0304c46bf2c
kahosan <15116355657l@gamil.com>
2021-08-17 22:05:02 +0800

    Site updated: 2021-08-17 22:04:55

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,14 +1,1 @@
-(function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'G-KNGSESZXK7', 'auto');
-ga('send', 'pageview');
+!function(e,a,t,n,g,c){e.GoogleAnalyticsObject=n,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=+new Date,g=a.createElement(t),c=a.getElementsByTagName(t)[0],g.async=1,g.src="https://www.google-analytics.com/analytics.js",c.parentNode.insertBefore(g,c)}(window,document,"script","ga"),ga("create","G-KNGSESZXK7","auto"),ga("send","pageview");
