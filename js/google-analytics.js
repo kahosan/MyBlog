338bb77626d76c7d1bcd08432461961db92b3ab6
kahosan <15116355657l@gamil.com>
2021-08-20 22:53:02 +0800

    remove all file

--- js/google-analytics.js
+++
@@ -1,1 +0,0 @@
-!function(e,a,t,n,g,c){e.GoogleAnalyticsObject=n,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=+new Date,g=a.createElement(t),c=a.getElementsByTagName(t)[0],g.async=1,g.src="https://www.google-analytics.com/analytics.js",c.parentNode.insertBefore(g,c)}(window,document,"script","ga"),ga("create","G-KNGSESZXK7","auto"),ga("send","pageview");
